836f63b28904ef248ec0ec81f7c812730685748b
jessicambs <35975954+jessicambs@users.noreply.github.com>
2021-03-14 16:27:40 +0100

    Update calculadora_v1.py

--- Cap03/Lab02/calculadora_v1.py
+++ Cap03/Lab02/calculadora_v1.py
@@ -7,3 +7,34 @@
 print("\n******************* Python Calculator *******************")
 
 
+print("\n Selecione a operação desejada \n \n 1 - Soma \n 2 - Subtração \n 3 - Multiplicação\n 4 - Divisão ")
+
+selecao = int(input("\nDigite sua opção (1/2/3/4:)"))
+
+n1 = int(input("\nDigite o primeiro número: "))
+
+n2 = int(input("\nDigite o segundo número: "))
+
+
+# SOMA
+if selecao == 1:
+    soma =  n1+n2
+    print("\nResultado: ", soma)
+
+# SUBTRAÇÃO
+elif selecao == 2:
+    subtracao =  n1-n2
+    print("\nResultado: ", subtracao)
+
+# MULTIPLICAÇÃO
+elif selecao == 3:
+    multiplicacao =  n1*n2
+    print("\nResultado: ", multiplicacao)
+
+# DIVISAO
+elif selecao == 4:
+    divisao =  n1//n2
+    print("\nResultado: ", divisao)
+
+else:
+    print("\nSeleção inválida!")
